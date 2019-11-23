323081eb84bcc617e86ebc1b6f084a681bd6be21
Florian  Bürgi <florian-buergi@iet-gibb.ch>
2019-11-24 00:24:51 +0100

    more stuff

--- src/main/java/ch/ffhs/pa5/latetrain/latetrainserver/controller/UserConnectionController.java
+++ src/main/java/ch/ffhs/pa5/latetrain/latetrainserver/controller/UserConnectionController.java
@@ -1,7 +1,11 @@
 package ch.ffhs.pa5.latetrain.latetrainserver.controller;
 
+import ch.ffhs.pa5.latetrain.latetrainserver.model.RouteResults;
 import ch.ffhs.pa5.latetrain.latetrainserver.model.UserConnection;
 import ch.ffhs.pa5.latetrain.latetrainserver.service.UserConnectionService;
+import java.time.LocalTime;
+import java.util.List;
+import org.hibernate.annotations.GeneratorType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,5 +18,30 @@ public class UserConnectionController {
     @PostMapping("/userConnection")
     public UserConnection createUserConnection(@RequestBody UserConnection userConnection){
         return userConnectionService.createUserConnection(userConnection);
+    }
+
+    @GetMapping("/userConnection/{id}/effectiveRoutes")
+    public RouteResults getEffectiveRoutesForConnection(@PathVariable Long id){
+        return userConnectionService.getEffectiveRoutesForUserConnectionWithId(id);
+    }
+
+    @PatchMapping("/userConnection/{id}/effectiveRoutes{routeIndex}")
+    public UserConnection updateAccordingToRoute(@PathVariable Long id, @PathVariable int routeIndex){
+        return userConnectionService.updateUserConnectionDetailsAccordingToEffectiveRoute(id, routeIndex);
+    }
+
+    @DeleteMapping("/userConnection/{id}")
+    public void deleteConnection(@PathVariable Long id){
+        userConnectionService.deleteUserConnectionById(id);
+    }
+
+    @GetMapping("/userConnection/{id}")
+    public UserConnection readById(@PathVariable Long id){
+        return userConnectionService.readUserConnectionById(id);
+    }
+
+    @GetMapping("/userConnection/all")
+    public List<UserConnection> readAll(){
+        return userConnectionService.readAllByCurrentUser();
     }
 }

99d54360ef977e8b495e90ef094532008be69368
Florian  Bürgi <florian-buergi@iet-gibb.ch>
2019-11-24 01:41:54 +0100

    some fixed

--- src/main/java/ch/ffhs/pa5/latetrain/latetrainserver/security/UserManagementService.java
+++ src/main/java/ch/ffhs/pa5/latetrain/latetrainserver/security/UserManagementService.java
@@ -11,19 +11,21 @@ import org.springframework.stereotype.Service;
 public class UserManagementService {
     @Autowired
     UserRepository userRepository;
+    @Autowired
+    BCryptPasswordEncoder passwordEncoder;
 
     public User register(User user) throws ValidationException {
-        if(userRepository.existsByUsername(user.getUsername())){
+        if(checkIfUserExists(user.getUsername())){
             throw new ValidationException("User exists");
         }
         if(!user.getPassword().equals(user.getPasswordConfirm())){
             throw new ValidationException("Password missmatch");
         }
-        user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
-    public boolean checkIfUserExists(String username){
+    private boolean checkIfUserExists(String username){
         return userRepository.existsByUsername(username);
     }
 
